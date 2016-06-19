73dfc917bbd60b98df167ba1c92155e595284b7d
nbpzjy <zjygzc@gmail.com>
2016-06-19 18:24:49 +0800

    third commit

--- app/src/main/java/com/nbpzjy/learnas/activitylearn/MainActivity.java
+++ app/src/main/java/com/nbpzjy/learnas/activitylearn/MainActivity.java
@@ -1,9 +1,12 @@
 package com.nbpzjy.learnas.activitylearn;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+
+import java.sql.SQLOutput;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,11 +16,56 @@ public class MainActivity extends AppCompatActivity {
 //        setContentView(R.layout.activity_main);
         setContentView(R.layout.my_layout);
 
+        System.out.println("onCreate");
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        System.out.println("onStart");
+    }
+
+    @Override
+            protected void onResume() {
+        super.onResume();
+        System.out.println("onResume");
+    }
+
+    @Override
+            protected void onPause() {
+        super.onPause();
+        System.out.println("onPause");
+    }
+
+    @Override
+            protected void onStop() {
+        super.onStop();
+        System.out.println("onStop");
+    }
+
+    @Override
+            protected void onDestroy() {
+        super.onDestroy();
+        System.out.println("onDestroy");
+    }
+
+
+
+    @Override
+            protected  void onRestart() {
+        super.onRestart();
+        System.out.println("onRestart");
+
+
+
+
+
         findViewById(R.id.btnStartAnotherAty).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                   startActivity(new Intent(MainActivity.this,AnotherAty.class));
-//                startActivity(new Intent(Intent.ACTION, Url.parse("http://wwww.nbpilot.com"));
+//                   startActivity(new Intent(MainActivity.this,AnotherAty.class));
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.nbpilot.com")));
 
             }
         });
